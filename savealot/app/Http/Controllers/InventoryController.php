0297d8f49123eab001612a274371a6607ec91b0b
jacobmccallum16 <118015063+jacobmccallum16@users.noreply.github.com>
2023-03-22 00:47:54 -0700

    Support for the Cart page

--- savealot/app/Http/Controllers/InventoryController.php
+++ savealot/app/Http/Controllers/InventoryController.php
@@ -1,10 +1,7 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use App\Models\Inventory;
 use Illuminate\Http\Request;
-
 class InventoryController extends Controller
 {
     /**
@@ -16,7 +13,6 @@ class InventoryController extends Controller
         $inventory = inventory::all();
         return view('inventory',['inventory' => $inventory]);
     }
-
     // Custom made
     public function shop()
     {
@@ -40,7 +36,6 @@ class InventoryController extends Controller
         //
         return view('/inventory/create');
     }
-
     /**
      * Store a newly created resource in storage.
      */
@@ -49,25 +44,34 @@ class InventoryController extends Controller
         // validate the form data
         $this->validate($request, [
             'prod_name' => 'required|max:255',
+            'prod_description' => 'max:255'
         ]);
         // process the data and submit it
         $inventory = new Inventory(); // this is the model Inventory
         $inventory->id = $request->id;
         $inventory->prod_name = $request->prod_name;
+        $inventory->prod_description = $request->prod_description;
+        $inventory->prod_purchase_price = $request->prod_purchase_price;
+        $inventory->prod_selling_price = $request->prod_selling_price;
+        $inventory->prod_units = $request->prod_units;
+        $inventory->prod_size = $request->prod_size;
+        $inventory->prod_quantity = $request->prod_quantity;
+        $inventory->prod_exp_date = $request->prod_exp_date;
+        $inventory->prod_picture = $request->prod_picture;
+
+
 
         // if successful we want to redirect
         if ($inventory->save()) {
-            $inventory = inventory::all();
-            return view('shop',['inventory' => $inventory]);
+            return redirect()->action([InventoryController::class, 'shop']);
         } else {
-            return view('/inventory/create');
+            return redirect('/inventory/create');
         }
     }
 
     /**
      * Display the specified resource.
      */
-
     public function show(string $id)
     {
         // find the record
@@ -75,7 +79,6 @@ class InventoryController extends Controller
         // return this view
         return view('inventory.show')->with('item', $inventory);
     }
-
     /**
      * Show the form for editing the specified resource.
      */
@@ -86,13 +89,36 @@ class InventoryController extends Controller
         // return this view
         return view('inventory.edit')->with('item', $inventory);
     }
-
     /**
      * Update the specified resource in storage.
      */
     public function update(Request $request, inventory $inventory)
     {
-        //
+        // validate the form data
+        $this->validate($request, [
+            'prod_name' => 'required|max:255',
+            'prod_description' => 'max:255'
+        ]);
+        // process the data and submit it
+        $inventory = new Inventory(); // this is the model Inventory
+        $inventory->id = $request->id;
+        $inventory->prod_name = $request->prod_name;
+        $inventory->prod_description = $request->prod_description;
+        $inventory->prod_purchase_price = $request->prod_purchase_price;
+        $inventory->prod_selling_price = $request->prod_selling_price;
+        $inventory->prod_units = $request->prod_units;
+        $inventory->prod_size = $request->prod_size;
+        $inventory->prod_quantity = $request->prod_quantity;
+        $inventory->prod_exp_date = $request->prod_exp_date;
+        $inventory->prod_picture = $request->prod_picture;
+
+
+        // if successful we want to redirect
+        if ($inventory->save()) {
+            return redirect()->action([InventoryController::class, 'shop']);
+        } else {
+            return redirect('/inventory');
+        }
     }
 
     /**
