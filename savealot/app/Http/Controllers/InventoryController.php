5b9cc1bd0c555c8707bc3e04042746cbcebbd8e2
jacobmccallum16 <118015063+jacobmccallum16@users.noreply.github.com>
2023-03-23 16:47:28 -0700

    Search enhancement–checks description as well now

--- savealot/app/Http/Controllers/InventoryController.php
+++ savealot/app/Http/Controllers/InventoryController.php
@@ -24,28 +24,25 @@ class InventoryController extends Controller
         $inventory = inventory::all();
         return view('shop',['inventory' => $inventory]);
     }
-<<<<<<< HEAD
-    
-=======
->>>>>>> 930ae708b5954bfdb668f2a3d73507a181528062
+
     public function search(Request $request)
     {
-        $inventory = Inventory::where('prod_name', 'like', '%' . request('search') . '%')->get();
+        $inventory = Inventory::where('prod_name', 'LIKE', '%' . request('search') . '%')->orWhere('prod_description', 'LIKE', '%' . request('search') . '%')->get();
         return view('/shop')->with('inventory', $inventory);
     }
-    
+
     public function addToCart(string $id)
     {
         session()->increment("$id");
         return redirect()->action([InventoryController::class, 'shop']);
     }
-    
+
     public function removeFromCart(string $id)
     {
         session()->decrement("$id");
         return redirect()->action([InventoryController::class, 'shop']);
     }
-    
+
     public function cart()
     {
         //
