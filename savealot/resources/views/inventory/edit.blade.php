782e90a30c9cb7fe18991b8dba3cf07ae676d0c7
jacobmccallum16 <118015063+jacobmccallum16@users.noreply.github.com>
2023-03-22 20:05:24 -0700

    Bootstrap CSS fixes

--- savealot/resources/views/inventory/edit.blade.php
+++ savealot/resources/views/inventory/edit.blade.php
@@ -5,7 +5,7 @@
 
 @section('main')
 <h1>Inventory</h1>
-<div class="row g-3">
+<div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-3 row-cols-xxl-4 g-3">
 	{{-- @foreach ($inventory as $item) --}}
         <div class="col-md-3 d-lg-none"></div>
 
@@ -45,67 +45,67 @@
 
         <div class="col-12 col-lg-8 col-xxl-9" id="Model" tabindex="-1">
             <div class="modal-dialog">
-                <form action="">
+                <form method="POST" action="edit">
                     <div class="modal-content">
                         <div class="modal-header">
                             <h1 class="modal-title fs-5" id="{{$item->id}}ModelLabel">Edit Product</h1>
                         </div>
                         <div class="modal-body row">
                             <div class="mb-1 mb-sm-3 col-3 col-sm-4">
-                                <label for="id-{{$item->id}}">ID</label>
-                                <input id="id-{{$item->id}}" type="number" class="form-control rounded-3" name="id-{{$item->id}}" value="{{$item->id}}" required>
+                                <label for="id">ID</label>
+                                <input id="id" type="number" class="form-control rounded-3" name="id" value="{{$item->id}}" required>
                             </div>
                             <div class="mb-1 mb-sm-3 col-9 col-sm-8">
-                                <label for="id-{{$item->id}}">Name</label>
-                                <input id="prod_name-{{$item->id}}" type="text" class="form-control rounded-3" name="prod_name-{{$item->id}}" value="{{$item->prod_name}}" required>
+                                <label for="prod_name">Name</label>
+                                <input id="prod_name" type="text" class="form-control rounded-3" name="prod_name" value="{{$item->prod_name}}" required>
                             </div>
                             <div class="mb-1 mb-sm-3 col-12 col-sm-12">
-                                <label for="prod_description-{{$item->id}}">Description</label>
-                                <textarea name="prod_description-{{$item->id}}" id="prod_description-{{$item->id}}" rows="3" class="form-control rounded-3">{{$item->prod_description}}</textarea>
+                                <label for="prod_description">Description</label>
+                                <textarea name="prod_description" id="prod_description" rows="3" class="form-control rounded-3">{{$item->prod_description}}</textarea>
                             </div>
                             <div class="mb-1 mb-sm-3 col-6 col-sm-6">
-                                <label for="prod_purchase_price-{{$item->id}}">Purchase Price</label>
+                                <label for="prod_purchase_price">Purchase Price</label>
                                 <div class="input-group rounded-3 col">
                                     <span class="input-group-text">$</span>
-                                    <input id="prod_purchase_price-{{$item->id}}" type="text" class="form-control" name="prod_purchase_price-{{$item->id}}" value="{{$item->prod_purchase_price}}">
+                                    <input id="prod_purchase_price" type="text" class="form-control" name="prod_purchase_price" value="{{$item->prod_purchase_price}}">
                                 </div>
                             </div>
                             <div class="mb-1 mb-sm-3 col-6 col-sm-6">
-                                <label for="prod_selling_price-{{$item->id}}">Selling Price</label>
+                                <label for="prod_selling_price">Selling Price</label>
                                 <div class="input-group rounded-3 col">
                                     <span class="input-group-text">$</span>
-                                    <input id="prod_selling_price-{{$item->id}}" type="text" class="form-control" name="prod_selling_price-{{$item->id}}" value="{{$item->prod_selling_price}}">
+                                    <input id="prod_selling_price" type="text" class="form-control" name="prod_selling_price" value="{{$item->prod_selling_price}}">
                                 </div>
                             </div>
                             <div class="mb-1 mb-sm-3 col-6 col-sm-4">
-                                <label for="prod_units-{{$item->id}}" class="center">Units</label>
+                                <label for="prod_units" class="center">Units</label>
                                 <div class="input-group rounded-3">
-                                    <input id="prod_units-{{$item->id}}" type="text" class="form-control" name="prod_units-{{$item->id}}" value="{{$item->prod_units}}">
+                                    <input id="prod_units" type="text" class="form-control" name="prod_units" value="{{$item->prod_units}}">
                                 </div>
                             </div>
                             <div class="mb-1 mb-sm-3 col-6 col-sm-4">
-                                <label for="prod_size-{{$item->id}}">Size</label>
+                                <label for="prod_size">Size</label>
                                 <div class="input-group rounded-3">
-                                    <input id="prod_size-{{$item->id}}" type="number" class="form-control" name="prod_size-{{$item->id}}" value="{{$item->prod_size}}">
+                                    <input id="prod_size" type="number" class="form-control" name="prod_size" value="{{$item->prod_size}}">
                                     <span class="input-group-text">g</span>
                                 </div>
                             </div>
                             <div class="mb-1 mb-sm-3 col-4 col-sm-4">
-                                <label for="prod_quantity-{{$item->id}}">Quantity</label>
+                                <label for="prod_quantity">Quantity</label>
                                 <div class="input-group rounded-3">
-                                    <input id="prod_quantity-{{$item->id}}" type="number" class="form-control" name="prod_quantity-{{$item->id}}" value="{{$item->prod_size}}">
+                                    <input id="prod_quantity" type="number" class="form-control" name="prod_quantity" value="{{$item->prod_size}}">
                                 </div>
                             </div>
                             <div class="mb-1 mb-sm-3 col-8 col-sm-4">
-                                <label for="prod_quantity-{{$item->id}}">Expiry Date</label>
+                                <label for="prod_quantity">Expiry Date</label>
                                 <div class="input-group rounded-3">
-                                    <input id="prod_exp_date-{{$item->id}}" type="date" class="form-control" name="prod_exp_date-{{$item->id}}" value="{{$item->prod_exp_date}}">
+                                    <input id="prod_exp_date" type="date" class="form-control" name="prod_exp_date" value="{{$item->prod_exp_date}}">
                                 </div>
                             </div>
                             <div class="mb-1 mb-sm-3 col-12 col-sm-8">
-                                <label for="prod_picture-{{$item->id}}">Picture URL</label>
+                                <label for="prod_picture">Picture URL</label>
                                 <div class="input-group rounded-3">
-                                    <input id="prod_picture-{{$item->id}}" type="text" class="form-control" name="prod_picture-{{$item->prod_picture}}" value="{{$item->prod_picture}}">
+                                    <input id="prod_picture" type="text" class="form-control" name="prod_picture" value="{{$item->prod_picture}}">
                                 </div>
                             </div>
                             <div class="mb-1 mb-sm-3 col-12 col-sm-6">
