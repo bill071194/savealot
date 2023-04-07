72bbedacf62439c5d8a997e4df1b5e14c5a6a568
jacobmccallum16 <118015063+jacobmccallum16@users.noreply.github.com>
2023-04-06 20:00:27 -0700

    Orders dashboard update

--- savealot/resources/views/orders.blade.php
+++ savealot/resources/views/orders.blade.php
@@ -1,59 +1,55 @@
 @extends('layouts.baseAdmin')
 
-@section('title', 'Orders')
+@section('title', 'Save-a-lot Admin')
 @section('adminTitle', 'Orders')
 @section('activeOrders', 'active')
 
 @section('section')
-<canvas class="my-4 w-100" id="myChart" width="1430" height="603" style="display: block; box-sizing: border-box; height: 301px; width: 715px;"></canvas>
+<div class="mb-4 border-bottom">
+    <div class="btn-group w-100 justify-content-center">
+        <form class="" action="" method="get">
+            <input type="hidden" name="dashboardDates" value="last7d">
+            <input type="submit" class="btn rounded-end-0 @if (session('dashboardDates') == 'last7d') btn-primary @else btn-outline-primary @endif" value="Last 7 days">
+        </form>
+        <form class="" action="" method="get">
+            <input type="hidden" name="dashboardDates" value="last30d">
+            <input type="submit" class="btn rounded-0 @if (session('dashboardDates') == 'last30d') btn-primary @else btn-outline-primary @endif" value="Last 30 days">
+        </form>
+        <form class="" action="" method="get">
+            <input type="hidden" name="dashboardDates" value="last12m">
+            <input type="submit" class="btn rounded-0 @if (session('dashboardDates') == 'last12m') btn-primary @else btn-outline-primary @endif" value="Last 12 months">
+        </form>
+        <form class="" action="" method="get">
+            <input type="hidden" name="dashboardDates" value="last5y">
+            <input type="submit" class="btn rounded-start-0 @if (session('dashboardDates') == 'last5y') btn-primary @else btn-outline-primary @endif" value="Last 5 years">
+        </form>
+    </div>
+    <div class="text-center w-100 fw-bold my-2">Revenue Chart</div>
+    <canvas class="my-2 w-100" id="revenueChart" width="1430" height="603" style="display: block; box-sizing: border-box; height: 301px; width: 715px;"></canvas>
+</div>
 
-<script>
-		/* globals Chart:false, feather:false */
-(() => {
-	'use strict'
-
-	feather.replace({ 'aria-hidden': 'true' })
-
-	// Graphs
-	const ctx = document.getElementById('myChart')
-	// eslint-disable-next-line no-unused-vars
-	const myChart = new Chart(ctx, {
-		type: 'line',
-		data: {
-			labels: [
-                @foreach ($orders->groupBy('date') as $order)
-                    "{{$order->first()->date}}",
-                @endforeach
-			],
-			datasets: [{
-				data: [
-                    @foreach ($orders->groupBy('date') as $order)
-                        {{number_format($order->sum('total'),2)}},
-                    @endforeach
-				],
-				lineTension: 0.25,
-				backgroundColor: 'green',
-				borderColor: 'black',
-				borderWidth: 4,
-				pointBackgroundColor: 'green'
-			}]
-		},
-		options: {
-			plugins: {
-				legend: {
-					display: false
-				},
-				tooltip: {
-					boxPadding: 3
-				}
-			}
-		}
-	})
-})()
-</script>
 <div class="row">
-    @foreach ($orders->sortByDesc('id') as $order)
-        <div class="col-12 col-xl-6">
+    <table class="table table-light table-striped table-bordered border-dark-subtle table-hover">
+        <thead>
+            <tr>
+                <th>Date</th>
+                <th>Orders</th>
+                <th>Revenue</th>
+            </tr>
+        </thead>
+        <tbody class="table-group-divider">
+            @foreach (array_reverse($ordersGrouped) as $orderGroup)
+                <tr>
+                    <td>{{$orderGroup['date']}}</td>
+                    <td>{{$orderGroup['orders']}}</td>
+                    <td>${{number_format($orderGroup['revenue'], 2, '.', ',')}}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+    {{-- @foreach ($orders->sortByDesc('id') as $order)
+        <div>Order ID: {{$order->id}}</div>
+        <div>
             <table class="table table-light table-striped table-bordered border-dark-subtle table-hover">
                 <thead>
                     <tr class="table-dark">
@@ -97,6 +93,34 @@
                 </tfoot>
             </table>
         </div>
-    @endforeach
+    @endforeach --}}
 </div>
+
+<script>
+	new Chart(document.getElementById('revenueChart'), {
+		type: 'line',
+		data: {
+			labels: [@foreach ($ordersGrouped as $order) "{{$order['date']}}", @endforeach],
+			datasets: [{
+				data: [@foreach ($ordersGrouped as $order) {{$order['revenue']}}, @endforeach],
+				lineTension: 0.25,
+				backgroundColor: 'green',
+				borderColor: 'green',
+				borderWidth: 4,
+				pointBackgroundColor: 'yellow'
+			}]
+		},
+		options: {
+			plugins: {
+				legend: {
+					display: false
+				},
+				tooltip: {
+					boxPadding: 3
+				}
+			}
+		}
+	});
+</script>
+
 @endsection
