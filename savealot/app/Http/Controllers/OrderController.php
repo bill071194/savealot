c7728eb80b72eca07a4de1f19c2a0c6843486544
jacobmccallum16 <118015063+jacobmccallum16@users.noreply.github.com>
2023-04-09 00:01:48 -0700

    Update OrderController.php

--- savealot/app/Http/Controllers/OrderController.php
+++ savealot/app/Http/Controllers/OrderController.php
@@ -12,47 +12,26 @@ use DB;
 
 class OrderController extends Controller
 {
-    public function checkOut(Request $request) {
+        public function checkOut(Request $request) {
+
         $user_id = $request->user_id;
         $inventory = inventory::all();
         $subtotal = 0;
         $discount = 0;
         $total = 0;
-
+        // check if order can be executed
         foreach ($inventory as $item) {
-            if (session("cart-$item->id") > 0) {
-                $prod_name = $item->prod_name;
-                $prod_picture = $item->prod_picture;
-                $prod_price = $item->prod_selling_price;
-                $item_qty = session("cart-$item->id");
-                $item_total = $prod_price * $item_qty;
-                $subtotal += $item_total;
+            if (session("cart-$item->id") > $item->prod_quantity) {
+                $difference = session("cart-$item->id") - $item->prod_quantity;
+                session(["cart-$item->id" => $item->prod_quantity]);
+                session(['stop-checkout' => 'true']);
+                session()->flash('cart-update', 'true');
+                session()->flash("cart-update-$item->id", 'true');
+                session()->flash("cart-update-$item->id-message", "Sorry, our stock of $item->prod_name has reduced and we have removed $difference of them from your cart.");
             }
         }
-
-        if ($request->student == 1) {
-            $student = 1;
-            $discount = number_format($subtotal * 0.1, 2, '.', '');
-            $total = $subtotal - $discount;
-        } else {
-            $student = 0;
-            $total = $subtotal;
-        }
-
-        $order = new Order();
-        $order->user_id = $user_id;
-        $order->subtotal = $subtotal;
-        $order->total = $total;
-        $order->discount = $discount;
-        $order->student = $student;
-
-        // $DATE = Carbon::today()->subDays(0)->toDateTimeString();    // TEMPORARY FOR BACKDATED ORDERS
-        // $order->created_at = $DATE;                                 // TEMPORARY FOR BACKDATED ORDERS
-
-        if ($order->save()) {
-            // session('status') = 'Purchase Successful!';
-            $row = Order::all()->sortBy('id')->last();
-            $order_id = $row['id'];
+        // give up and go back to cart page if order is not valid
+        if (session('stop-checkout') != 'true') {
             foreach ($inventory as $item) {
                 if (session("cart-$item->id") > 0) {
                     $prod_name = $item->prod_name;
@@ -60,29 +39,67 @@ class OrderController extends Controller
                     $prod_price = $item->prod_selling_price;
                     $item_qty = session("cart-$item->id");
                     $item_total = $prod_price * $item_qty;
-                    // Transaction
-                    $transaction = new Transaction();
-                    $transaction->user_id = $user_id;
-                    $transaction->order_id = $order_id;
-                    $transaction->prod_id = $item->id;
-                    $transaction->prod_name = $prod_name;
-                    $transaction->prod_picture = $prod_picture;
-                    $transaction->prod_price = $prod_price;
-                    $transaction->item_qty = $item_qty;
-                    $transaction->item_total = $item_total;
-                    // $transaction->created_at = $DATE;   // TEMPORARY FOR BACKDATED ORDERS
-                    $transaction->save();
-                    session(["cart-$item->id" => 0]);
-                    // Inventory
-                    $item->prod_quantity -= $item_qty;
-                    $item->prod_sold += $item_qty;
-                    $item->prod_revenue += $item_total;
-                    $item->update();
+                    $subtotal += $item_total;
                 }
             }
-            return redirect('/orderhistory');
-        } else {
-            return redirect('/cart');
+
+            if ($request->student == 1) {
+                $student = 1;
+                $discount = number_format($subtotal * 0.1, 2, '.', '');
+                $total = $subtotal - $discount;
+            } else {
+                $student = 0;
+                $total = $subtotal;
+            }
+
+            $order = new Order();
+            $order->user_id = $user_id;
+            $order->subtotal = $subtotal;
+            $order->total = $total;
+            $order->discount = $discount;
+            $order->student = $student;
+
+            // $DATE = Carbon::today()->subDays(0)->toDateTimeString();    // TEMPORARY FOR BACKDATED ORDERS
+            // $order->created_at = $DATE;                                 // TEMPORARY FOR BACKDATED ORDERS
+
+            if ($order->save()) {
+                // session('status') = 'Purchase Successful!';
+                $row = Order::all()->sortBy('id')->last();
+                $order_id = $row['id'];
+                foreach ($inventory as $item) {
+                    if (session("cart-$item->id") > 0) {
+                        $prod_name = $item->prod_name;
+                        $prod_picture = $item->prod_picture;
+                        $prod_price = $item->prod_selling_price;
+                        $item_qty = session("cart-$item->id");
+                        $item_total = $prod_price * $item_qty;
+                        // Transaction
+                        $transaction = new Transaction();
+                        $transaction->user_id = $user_id;
+                        $transaction->order_id = $order_id;
+                        $transaction->prod_id = $item->id;
+                        $transaction->prod_name = $prod_name;
+                        $transaction->prod_picture = $prod_picture;
+                        $transaction->prod_price = $prod_price;
+                        $transaction->item_qty = $item_qty;
+                        $transaction->item_total = $item_total;
+                        // $transaction->created_at = $DATE;   // TEMPORARY FOR BACKDATED ORDERS
+                        $transaction->save();
+                        session(["cart-$item->id" => 0]);
+                        // Inventory
+                        $item->prod_quantity -= $item_qty;
+                        $item->prod_sold += $item_qty;
+                        $item->prod_revenue += $item_total;
+                        $item->update();
+                    }
+                }
+                return redirect('/orderhistory');
+            } else {
+                return redirect('/cart');
+            }
+        }else {
+            session(['stop-checkout' => 'false']);
+            return back()->withInput();
         }
     }
 
